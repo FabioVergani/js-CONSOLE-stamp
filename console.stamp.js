f418dd433db24748f46f754db1534826b2556fe5
FabioVergani <fabio.vergani.1975@gmail.com>
2014-05-30 20:24:29 +0200

    Update console.stamp.js

--- console.stamp.js
+++ console.stamp.js
@@ -8,9 +8,8 @@ Array.SliceOn=function(x){return Array.prototype.slice.call(x)};
 
 
 //console.stamp=function(s){
- var o=console.stamp,t=new Date(),
- z=t.getTime(),m=Array.SliceOn(arguments),l=':',
- f=function(s,i){s=String(t['get'+s]());return s.length===i?'0'+s:s};
+console.stamp=function(s){
+ var o=console.stamp,t=new Date(),m=Array.SliceOn(arguments),l=':',f=function(s,i){s=String(t['get'+s]());return s.length===i?'0'+s:s};
  m.unshift([f('Hours',1),l,f('Minutes',1),l,f('Seconds',1),l,f('Milliseconds',2),'|',t-o.t2||0,'\t'].contiguous);
  console.log.apply(this,m);
  o.t2=t;
